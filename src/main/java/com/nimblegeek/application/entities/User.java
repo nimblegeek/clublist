70a3c1fdc305da43ebed1a60ed941b289dbbfa91
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-05-15 15:16:28 +0200

    Initial commit

---
+++ src/main/java/com/nimblegeek/application/entities/User.java
@@ -0,0 +1,4 @@
+package com.nimblegeek.application.entities;
+
+public class User {
+}
---
+++ src/test/java/com/nimblegeek/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimblegeek.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
