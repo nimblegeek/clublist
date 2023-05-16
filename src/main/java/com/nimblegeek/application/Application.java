7e35af9bbc1a93033d8bc6f18eb7049280b688e1
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-05-16 14:19:31 +0200

    Bootstrapping the application

--- src/main/java/com/nimblegeek/application/Application.java
+++ src/main/java/com/nimblegeek/application/Application.java
@@ -1,7 +1,14 @@
 package com.nimblegeek.application;
 
+import com.nimblegeek.application.entities.User;
+import com.nimblegeek.application.repositories.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +17,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
+	}
 }
