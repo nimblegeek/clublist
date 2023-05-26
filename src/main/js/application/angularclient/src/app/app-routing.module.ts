1c3fb8cc12dfa8ce2a05195eda1f43596534c523
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-05-26 22:27:03 +0200

    Configuring module so requests can be dispatched to matching components.

--- src/main/js/application/angularclient/src/app/app-routing.module.ts
+++ src/main/js/application/angularclient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ClubListComponent } from './Club-list/Club-list.component';
+import { ClubFormComponent } from './Club-form/Club-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Clubs', component: ClubListComponent },
+  { path: 'addClub', component: ClubFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
