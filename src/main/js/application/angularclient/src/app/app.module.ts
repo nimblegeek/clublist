71138e70dfa7ffb3668d98c703c8c0491a65c02d
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-05-26 22:24:40 +0200

    Preparing the HTML form for persisting a new club in the database.

--- src/main/js/application/angularclient/src/app/app.module.ts
+++ src/main/js/application/angularclient/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClubListComponent } from './club-list/club-list.component';
 import { ClubFormComponent } from './club-form/club-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ClubFormComponent } from './club-form/club-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
