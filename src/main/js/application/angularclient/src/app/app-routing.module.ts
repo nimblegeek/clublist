1a3d1aec8a179b3f71b8fa089531149ebc17559c
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-06-01 15:35:04 +0200

    list and form components working now.

--- src/main/js/application/angularclient/src/app/app-routing.module.ts
+++ src/main/js/application/angularclient/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ClubListComponent } from './Club-list/Club-list.component';
-import { ClubFormComponent } from './Club-form/Club-form.component';
+import { ClubListComponent } from './club-list/club-list.component';
+import { ClubFormComponent } from './club-form/club-form.component';
 
 const routes: Routes = [
-  { path: 'Clubs', component: ClubListComponent },
+  { path: 'clubs', component: ClubListComponent },
   { path: 'addClub', component: ClubFormComponent }
 ];
 
--- src/main/js/application/angularclient/src/app/app.module.ts
+++ src/main/js/application/angularclient/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ClubListComponent} from './Club-list/Club-list.component';
-import { ClubFormComponent } from './Club-form/Club-form.component';
+import { ClubListComponent} from './club-list/club-list.component';
+import { ClubFormComponent } from './club-form/club-form.component';
 import { ClubService } from './service/club-service.service';
 
 @NgModule({
