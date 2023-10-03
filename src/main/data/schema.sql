58c623d0398cd0ac87f99a3a6357f0e563112516
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-10-03 22:35:08 +0200

    Updating schema to include address field.

--- src/main/data/schema.sql
+++ src/main/data/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE clubs (
                        id SERIAL PRIMARY KEY,
-                       name VARCHAR(100) NOT NULL,
-                       status INT
+                       name VARCHAR(100) NOT NULL UNIQUE,
+                       address TEXT,  -- You can use TEXT data type for the address, or use separate columns for street, city, state, etc., depending on your requirements.
+                       status VARCHAR(50)  -- Use a more descriptive data type for status.
 );
